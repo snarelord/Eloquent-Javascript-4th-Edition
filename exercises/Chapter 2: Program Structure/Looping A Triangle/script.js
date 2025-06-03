e08a6b6255f3f6e7118f29079d275007d926595f
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-06-03 12:43:01 +0100

    complete chapter 2 exercises

--- exercises/Chapter 2: Program Structure/Looping A Triangle/script.js
+++ exercises/Chapter 2: Program Structure/Looping A Triangle/script.js
@@ -1,3 +1,22 @@
+// “Write a loop that makes seven calls to console.log to output the following triangle:
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+// It may be useful to know that you can find the length of a string by writing .length after it.
+// let abc = "abc";
+// console.log(abc.length);
+// // → 3”
+
+// Excerpt From
+// Eloquent Javascript, 4th Edition
+// Marijn Haverbeke
+// This material may be protected by copyright.
+
 const body = document.body;
 for (let i = "#"; i.length < 8; i += "#") {
   const line = document.createElement("tr");
