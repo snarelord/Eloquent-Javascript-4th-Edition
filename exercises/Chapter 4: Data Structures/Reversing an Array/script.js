17dbddfcc18c0d4cb3d1c94ad259e1ffe985ec88
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-08-22 11:01:18 +0100

    Chapter 4 exercises

--- exercises/Chapter 4: Data Structures/Reversing an Array/script.js
+++ exercises/Chapter 4: Data Structures/Reversing an Array/script.js
@@ -4,6 +4,33 @@
 
 // Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one runs faster?"
 
-function reverseArray(arr) {}
+function reverseArray(arr) {
+  const reversed = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    // my condition was initially incorrect for a reverse loop, forgot I needed to stop at 0 and not arr.length
+    reversed.push(arr[i]);
+  }
+  return reversed;
+}
+console.log(reverseArray([1, 2, 4, 6, 7]));
 
-function reverseArrayInPlace(arr) {}
+function reverseArrayInPlace(arr) {
+  // need to mutate the array passed in so elements are in reverse order
+  // cannot return a new array, needs to be mutated
+  // two pointer approach
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start < end) {
+    // remember how you can swap elements USE TEMP
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+
+    start++;
+    end--;
+  }
+}
+let arrValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrValue);
+console.log(arrValue);
